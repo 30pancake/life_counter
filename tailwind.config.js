6574e1b76ba29d26aa10eec4c593c2a5f468c8f3
DESKTOP-OAN49KF\rgm_7 <kenkyukenkyu@gmail.com>
2023-07-17 13:35:01 +0900

    tailwindを導入

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/index.vue",
+    "./components/counters_source_view.vue",
+    "./components/creatures_source_view.vue",
+    "./components/creatures_unit_list_view.vue",
+    "./components/creatures_unit_view.vue",
+    "./components/player_info_view.vue",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
